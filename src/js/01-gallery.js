a8600300179b914350563de048ec8bdab0bf2678
andrpoch <andrpoch25@gmail.com>
2022-02-07 18:09:44 +0200

    Finish second task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+   
+const markup = galleryItems.map(({ description, original, preview }) => 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+   .join('');
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener(`click`, event => event.preventDefault());
+
+const captionOptions = {
+    captionSelector:'img',
+    captionType:'attr',
+    captionsData:'alt',
+    captionPosition:'bottom',
+    captionDelay: 250,
+    uniqueImages: true,
+   };
+
+let lightbox = new SimpleLightbox('.gallery a', captionOptions);
