0e78417405af8ef341b0a359c1d4c239e1446a81
andrpoch <andrpoch25@gmail.com>
2022-02-09 14:39:39 +0200

    Update 03-feedback.js
    add2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,21 @@
 import throttle from 'lodash.throttle';
+import * as storage from '../services/localStorage';
+
 
 const form = document.querySelector('form');
 
 form.addEventListener('input',throttle(inputHandler,500));
+const STORAGE_KEY = 'feedback-form-state';
 
 function inputHandler (e) {
-   const { name, type, value } = e.target;
+   const { name, value } = e.target;
    if (name === 'password') return;
-}
+
+   const parsedData = storage.get(STORAGE_KEY) ?? {};
+
+   const formData = {
+      ...parsedData,
+      [name]: value,
+   };
+   storage.save(STORAGE_KEY, formData)
+};
