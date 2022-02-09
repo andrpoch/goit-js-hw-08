46cfd6a98e4fb54e4b708655ff4264ccf9025671
andrpoch <andrpoch25@gmail.com>
2022-02-09 13:49:04 +0200

    Do commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+
+form.addEventListener('input',throttle(onInput,500));
+
+function onInput (e) {
+   const formData = {};
+   formData[e.target.name] = e.target.value;
+   console.log(formData);
+}
