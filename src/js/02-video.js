a05d3168c17fcedc1c67dba80bd9931ab92974b7
andrpoch <andrpoch25@gmail.com>
2022-02-09 17:49:14 +0200

    Fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,16 @@ import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 
-const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
 const iframePlayer = new Vimeo.Player(iframe);
 
 function dataEvent (data){
-   localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+   const currentTime = data.seconds;
+   localStorage.setItem("videoplayer-current-time", currentTime);
 };
 
 iframePlayer.on('timeupdate',throttle(dataEvent,1000)); 
 iframePlayer.setCurrentTime(currentTime());
 function currentTime() {
-   return JSON.parse(localStorage.getItem('videoplayer-current-time')).seconds
+   return JSON.parse(localStorage.getItem('videoplayer-current-time'))
 };
-
