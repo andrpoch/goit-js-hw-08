29b8c047b86754b35f38b9558e033c657e4d9a45
andrpoch <andrpoch25@gmail.com>
2022-02-09 13:09:05 +0200

    Completed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+function dataEvent (data){
+   const currentTime = data.seconds;
+   localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+iframePlayer.on('timeupdate',throttle(dataEvent,1000)); 
+iframePlayer.setCurrentTime(currentTime());
+function currentTime() {
+   return JSON.parse(localStorage.getItem('videoplayer-current-time'))
+};
