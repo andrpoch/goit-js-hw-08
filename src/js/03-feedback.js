5b82555a362690b41d0c41971609edb5d53de6be
andrpoch <andrpoch25@gmail.com>
2022-02-09 14:08:00 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,9 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
 
-form.addEventListener('input',throttle(onInput,500));
+form.addEventListener('input',throttle(inputHandler,500));
 
-function onInput (e) {
-   const formData = {};
-   formData[e.target.name] = e.target.value;
-   console.log(formData);
+function inputHandler (e) {
+   const { name, type, value } = e.target;
+   if (name === 'password') return;
 }
