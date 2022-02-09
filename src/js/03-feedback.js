224884d86ad10cd499ea5a19bd98fd05dd0a770d
andrpoch <andrpoch25@gmail.com>
2022-02-09 15:29:26 +0200

    Completed third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,20 +2,46 @@ import throttle from 'lodash.throttle';
 import * as storage from '../services/localStorage';
 
 
-const form = document.querySelector('form');
-
-form.addEventListener('input',throttle(inputHandler,500));
+const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
 function inputHandler (e) {
    const { name, value } = e.target;
-   if (name === 'password') return;
 
-   const parsedData = storage.get(STORAGE_KEY) ?? {};
-
+   const parsedData = storage.get(STORAGE_KEY) || {};
    const formData = {
       ...parsedData,
       [name]: value,
    };
    storage.save(STORAGE_KEY, formData)
 };
+
+function rehydrateData() {
+   const parsedData = storage.get(STORAGE_KEY);
+   const {
+      elements: { email, message }
+   } = form;
+   email.value = parsedData?.email || '';
+   message.value = parsedData?.message || '';
+}
+rehydrateData();
+
+function submitHandler(e) {
+   e.preventDefault();
+   const form = e.currentTarget;
+   const formData = new FormData(form);
+   const finalData = {};
+
+   for (const [key, value] of formData.entries()) {
+      if (!value) {
+         alert('Quickly fill in all the fields!');
+         return;
+      }
+      finalData[key] = value;  
+   }
+   console.log(finalData);
+   form.reset();
+   storage.remove(STORAGE_KEY);
+};
+form.addEventListener('input',throttle(inputHandler,500));
+form.addEventListener('submit', submitHandler);
